7c27227252af03fd6aaf88b512a5b7029c919ac5
Julian Martinez <julian.martinez.cs@gmail.com>
2023-02-03 10:51:21 +0000

    basic express server with typescript

--- README.md
+++ README.md
@@ -2,8 +2,8 @@ Build TODO's CRUD rest API with typescript + Express.js + Sequelize + Sqlite3
 
 imporove performance using express-validator
 
-[] Set up typescript for dev
-[] basic express server with typescript
+[x] Set up typescript for dev
+[x] basic express server with typescript
 [] setup database
 [] create model
 [] create todo
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log('sup my dude?');
+import express, { Request, Response } from 'express'
+
+//Create port
+const port = 9000;
+
+//Create the app
+const app = express()
+
+//Test end-point
+app.get('/', (req: Request, res: Response) => {
+    return res.send('Hello World')
+})
+
+//App.listen
+app.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`)
+})
