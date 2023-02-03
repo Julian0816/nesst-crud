4472015fdf02202a838361ca244f1d589065ee06
Julian Martinez <julian.martinez.cs@gmail.com>
2023-02-03 15:06:01 +0000

    more clean up

---
+++ src/routes/index.ts
@@ -0,0 +1,59 @@
+import express from "express";
+import TodoValidator from "../validator";
+import middleware from "../middleware";
+import TodoController from "../controller";
+
+const router = express.Router();
+
+router.post(
+  "/create",
+  TodoValidator.checkCreateTodo(),
+  //   (req: Request, res: Response, next: NextFunction) => {
+  //     const error = validationResult(req);
+  //     if (!error.isEmpty()) {
+  //       return res.json(error);
+  //     }
+  //     next();
+  //   },
+  middleware.handleValidationError,
+  //   async (req: Request, res: Response) => {
+  //     const id = uuidv4();
+  //     try {
+  //       const record = await TodoInstance.create({ ...req.body, id });
+  //       return res.json({ record, msg: "Successfully create todo" });
+  //     } catch (e) {
+  //       return res.json({ msg: "fail to create", status: 500, route: "/create" });
+  //     }
+  //   }
+  TodoController.create
+);
+
+router.get(
+  "/read",
+  TodoValidator.checkReadTodo(),
+  middleware.handleValidationError,
+  TodoController.readPagination
+);
+
+router.get(
+  "/read/:id",
+  TodoValidator.checkIdParams(),
+  middleware.handleValidationError,
+  TodoController.readById
+);
+
+router.put(
+  "/update/:id",
+  TodoValidator.checkIdParams(),
+  middleware.handleValidationError,
+  TodoController.update
+);
+
+router.delete(
+  "/delete/:id",
+  TodoValidator.checkIdParams(),
+  middleware.handleValidationError,
+  TodoController.delete
+);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
 import db from "./config/database.config";
+import todoRouter from "./routes";
 // import { TodoInstance } from "./model/index";
 // import { v4 as uuidv4 } from "uuid";
-import TodoValidator from "./validator";
-import middleware from "./middleware";
-import TodoController from './controller';
+// import TodoValidator from "./validator";
+// import middleware from "./middleware";
+// import TodoController from './controller';
 
 //Sync database
 db.sync().then(() => {
@@ -20,61 +21,15 @@ const app = express();
 //Middleware
 app.use(express.json());
 
+//import router
+app.use('/api/v1', todoRouter)
+
 //Test end-point
 // app.get('/', (req: Request, res: Response) => {
 //     return res.send('This is still up and running')
 // })
 
-app.post(
-  "/create",
-  TodoValidator.checkCreateTodo(),
-  //   (req: Request, res: Response, next: NextFunction) => {
-  //     const error = validationResult(req);
-  //     if (!error.isEmpty()) {
-  //       return res.json(error);
-  //     }
-  //     next();
-  //   },
-  middleware.handleValidationError,
-//   async (req: Request, res: Response) => {
-//     const id = uuidv4();
-//     try {
-//       const record = await TodoInstance.create({ ...req.body, id });
-//       return res.json({ record, msg: "Successfully create todo" });
-//     } catch (e) {
-//       return res.json({ msg: "fail to create", status: 500, route: "/create" });
-//     }
-//   }
-    TodoController.create,
-);
 
-app.get(
-  "/read",
-  TodoValidator.checkReadTodo(),
-  middleware.handleValidationError,
-  TodoController.readPagination,
-);
-
-app.get(
-  "/read/:id",
-  TodoValidator.checkIdParams(),
-  middleware.handleValidationError,
-  TodoController.readById,
-);
-
-app.put(
-  "/update/:id",
-  TodoValidator.checkIdParams(),
-  middleware.handleValidationError,
-  TodoController.update,
-);
-
-app.delete(
-  "/delete/:id",
-  TodoValidator.checkIdParams(),
-  middleware.handleValidationError,
-  TodoController.delete,
-);
 
 //App.listen
 app.listen(port, () => {
